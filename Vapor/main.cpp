c8a80bb292c2ea2ca205cc74043f4d14652a777f
loic <910041@gmail.com>
2025-02-06 12:50:42 +0800

    feat: select gfx backends from CLI

--- Vapor/main.cpp
+++ Vapor/main.cpp
@@ -1,5 +1,6 @@
 #include "SDL.h"
 #include "fmt/core.h"
+#include "args.hxx"
 #include "renderer_metal.hpp"
 #include "renderer_vulkan.hpp"
 
@@ -20,16 +21,56 @@ Renderer* createRenderer(GraphicsBackend backend, SDL_Window* window) {
 }
 
 int main(int argc, char* args[]) {
+    args::ArgumentParser parser { "This is Project Vapor." };
+    args::Group group(parser, "Select a gfx backend:", args::Group::Validators::Xor);
+    args::Flag useMetal(group, "Metal", "Metal", {"metal"});
+    args::Flag useVulkan(group, "Vulkan", "Vulkan", {"vulkan"});
+    if (argc > 1) {
+        try {
+            parser.ParseCLI(argc, args);
+        } catch (args::Help) {
+            std::cout << parser;
+            return 0;
+        } catch (args::ParseError e) {
+            std::cerr << e.what() << std::endl;
+            std::cerr << parser;
+            return 1;
+        } catch (args::ValidationError e) {
+            std::cerr << e.what() << std::endl;
+            std::cerr << parser;
+            return 1;
+        }
+    }
+
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         fmt::print("SDL could not initialize! Error: {}\n", SDL_GetError());
         return 1;
     }
+
+    const char* winTitle;
+    Uint32 winFlags = SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE;
+    GraphicsBackend gfxBackend;
+#if defined(__APPLE__)
+    if (useVulkan || !useMetal) {
+        winTitle = "Project Vapor (Vulkan)";
+        winFlags |= SDL_WINDOW_VULKAN;
+        gfxBackend = GraphicsBackend::Vulkan;
+    } else {
+        winTitle = "Project Vapor (Metal)";
+        winFlags |= SDL_WINDOW_METAL;
+        gfxBackend = GraphicsBackend::Metal;
+    }
+#else
+    title = "Project Vapor (Vulkan)";
+    flags |= SDL_WINDOW_VULKAN;
+    backend = GraphicsBackend::Vulkan;
+#endif
+
     auto window = SDL_CreateWindow(
-      "MyApp", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600,
-      SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE | SDL_WINDOW_VULKAN
+        winTitle, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, winFlags
     );
 
-    auto renderer = createRenderer(GraphicsBackend::Vulkan, window);
+    auto renderer = createRenderer(gfxBackend, window);
     renderer->init();
 
     bool quit = false;
