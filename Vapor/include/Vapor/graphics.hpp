27fda036c5d0a5941d9745f1bd397093e6f7fdcd
loicchen <910041@gmail.com>
2025-12-07 09:04:47 +0800

    chore: check particle data alignment

--- Vapor/include/Vapor/graphics.hpp
+++ Vapor/include/Vapor/graphics.hpp
@@ -316,7 +316,6 @@ struct WaterVertexData {
     glm::vec2 uv1;// Grid UV for edge dampening
 };
 
-// GPU Particle for compute shader simulation
 struct alignas(16) GPUParticle {
     glm::vec3 position = glm::vec3(0.0f);
     float _pad1 = 0.0f;
@@ -327,18 +326,17 @@ struct alignas(16) GPUParticle {
     glm::vec4 color = glm::vec4(1.0f);
 };
 
-// Particle simulation parameters (uniform buffer)
+// TODO: check alignment
 struct alignas(16) ParticleSimulationParams {
     glm::vec2 resolution = glm::vec2(1280.0f, 720.0f);
-    glm::vec2 _pad1;
+    // glm::vec2 _pad1;
     glm::vec2 mousePosition = glm::vec2(0.0f);
-    glm::vec2 _pad2;
+    // glm::vec2 _pad2;
     float time = 0.0f;
     float deltaTime = 0.0f;
     Uint32 particleCount = 0;
 };
 
-// Attractor data for particle simulation
 struct alignas(16) ParticleAttractorData {
     glm::vec3 position = glm::vec3(0.0f);
     float _pad1;
